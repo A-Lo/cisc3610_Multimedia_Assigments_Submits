3c368532b479afa4161bcf51febf7e965c2c9951
Angelo Acevedo <angeloacevedo9@gmail.com>
2020-05-15 22:41:23 -0400

    Labs

---
+++ Labs/Lab_2_spriteNum/numbers.js
@@ -0,0 +1,233 @@
+numbers= {
+	src : "spritesheet.png",
+	img:undefined, 
+	frame:0,
+	offset:0,
+
+			
+				"frames": [
+					{
+						"filename": "0.png",
+						"frame": {
+							"x": 10,
+							"y": 10,
+							"w": 27,
+							"h": 48
+						},
+						"rotated": false,
+						"trimmed": false,
+						"spriteSourceSize": {
+							"x": 0,
+							"y": 0,
+							"w": 27,
+							"h": 48
+						},
+						"sourceSize": {
+							"w": 27,
+							"h": 48
+						}
+					},
+					{
+						"filename": "1.png",
+						"frame": {
+							"x": 57,
+							"y": 10,
+							"w": 27,
+							"h": 48
+						},
+						"rotated": false,
+						"trimmed": false,
+						"spriteSourceSize": {
+							"x": 0,
+							"y": 0,
+							"w": 27,
+							"h": 48
+						},
+						"sourceSize": {
+							"w": 27,
+							"h": 48
+						}
+					},
+					{
+						"filename": "2.png",
+						"frame": {
+							"x": 104,
+							"y": 10,
+							"w": 27,
+							"h": 48
+						},
+						"rotated": false,
+						"trimmed": false,
+						"spriteSourceSize": {
+							"x": 0,
+							"y": 0,
+							"w": 27,
+							"h": 48
+						},
+						"sourceSize": {
+							"w": 27,
+							"h": 48
+						}
+					},
+					{
+						"filename": "3.png",
+						"frame": {
+							"x": 151,
+							"y": 10,
+							"w": 27,
+							"h": 48
+						},
+						"rotated": false,
+						"trimmed": false,
+						"spriteSourceSize": {
+							"x": 0,
+							"y": 0,
+							"w": 27,
+							"h": 48
+						},
+						"sourceSize": {
+							"w": 27,
+							"h": 48
+						}
+					},
+					{
+						"filename": "4.png",
+						"frame": {
+							"x": 198,
+							"y": 10,
+							"w": 27,
+							"h": 48
+						},
+						"rotated": false,
+						"trimmed": false,
+						"spriteSourceSize": {
+							"x": 0,
+							"y": 0,
+							"w": 27,
+							"h": 48
+						},
+						"sourceSize": {
+							"w": 27,
+							"h": 48
+						}
+					},
+					{
+						"filename": "5.png",
+						"frame": {
+							"x": 245,
+							"y": 10,
+							"w": 27,
+							"h": 48
+						},
+						"rotated": false,
+						"trimmed": false,
+						"spriteSourceSize": {
+							"x": 0,
+							"y": 0,
+							"w": 27,
+							"h": 48
+						},
+						"sourceSize": {
+							"w": 27,
+							"h": 48
+						}
+					},
+					{
+						"filename": "6.png",
+						"frame": {
+							"x": 292,
+							"y": 10,
+							"w": 27,
+							"h": 48
+						},
+						"rotated": false,
+						"trimmed": false,
+						"spriteSourceSize": {
+							"x": 0,
+							"y": 0,
+							"w": 27,
+							"h": 48
+						},
+						"sourceSize": {
+							"w": 27,
+							"h": 48
+						}
+					},
+					{
+						"filename": "7.png",
+						"frame": {
+							"x": 339,
+							"y": 10,
+							"w": 27,
+							"h": 48
+						},
+						"rotated": false,
+						"trimmed": false,
+						"spriteSourceSize": {
+							"x": 0,
+							"y": 0,
+							"w": 27,
+							"h": 48
+						},
+						"sourceSize": {
+							"w": 27,
+							"h": 48
+						}
+					},
+					{
+						"filename": "8.png",
+						"frame": {
+							"x": 386,
+							"y": 10,
+							"w": 27,
+							"h": 48
+						},
+						"rotated": false,
+						"trimmed": false,
+						"spriteSourceSize": {
+							"x": 0,
+							"y": 0,
+							"w": 27,
+							"h": 48
+						},
+						"sourceSize": {
+							"w": 27,
+							"h": 48
+						}
+					},
+					{
+						"filename": "9.png",
+						"frame": {
+							"x": 433,
+							"y": 10,
+							"w": 27,
+							"h": 48
+						},
+						"rotated": false,
+						"trimmed": false,
+						"spriteSourceSize": {
+							"x": 0,
+							"y": 0,
+							"w": 27,
+							"h": 48
+						},
+						"sourceSize": {
+							"w": 27,
+							"h": 48
+						}
+					}
+				],
+				"meta": {
+					"app": "http://www.codeandweb.com/texturepacker",
+					"version": "1.0",
+					"image": "spritesheet.png",
+					"format": "RGBA8888",
+					"size": {
+						"w": 470,
+						"h": 68
+					},
+					"scale": "1"
+				}
+			}
+		
+			
---
+++ Labs/Lab_sound/audio.js
@@ -0,0 +1,9 @@
+function playAudio(id){
+	var audioID = document.getElementById(id);
+	audioID.play();
+}
+function pause(id){
+	var pauseID = document.getElementById(id);
+	pauseID.pause();
+}
+
